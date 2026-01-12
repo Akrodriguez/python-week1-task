14a53045858d44189bd116895e7ee5a560c92217
Ansh Kumar <anshkr11c@gmail.com>
2026-01-12 14:10:26 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,15 @@
+# Personal Introduction Program
+# Week 1 Task - Python Basics
+
+print(" Welcome to the Personal Introduction Program!\n")
+
+# Taking user inputs
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+city = input("Which city are you from? ")
+
+# Displaying friendly output
+print("\n Welcome " + name + "! ")
+print(f"You are {age} years old, from {city}, and you love {hobby}.")
+print("Nice to meet you! 😊")
